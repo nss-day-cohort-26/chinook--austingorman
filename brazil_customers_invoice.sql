f49d1af753d5bd695d17d61e277365831f8e9fc2
Austin Gorman <austin.gorman@gmail.com>
2018-09-21 12:26:49 -0500

    added brazil customer invoice

---
+++ brazil_customers_invoice.sql
@@ -0,0 +1,1 @@
+SELECT FirstName, LastName, Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry FROM Customer JOIN Invoice ON Customer.CustomerId=Invoice.CustomerId WHERE Country IS "Brazil";
